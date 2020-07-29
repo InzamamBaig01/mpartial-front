f2744fce537e55f6cd63e28faac514fd1580bc88
Your Name <you@example.com>
2020-07-29 23:11:04 +0500

    changes

--- client/app/pages/UserOrder/_components/MultipleSelectField.tsx
+++ client/app/pages/UserOrder/_components/MultipleSelectField.tsx
@@ -1,36 +1,55 @@
-import React, { useState } from "react";
-import MultiSelect from "react-multi-select-component";
+import React, { useState, useEffect } from 'react';
+import MultiSelect from 'react-multi-select-component';
 
 const MultipleSelectField = (props) => {
-    const [selected, setSelected] = useState([]);
-    const [valid, setValid] = useState(null);
-    const options = [];
-    props.field.options.map((option) => {
-      options.push({
-        label: option,
-        value: option,
-      });
+  console.log(props.field);
+  const [selected, setSelected] = useState([]);
+  const [valid, setValid] = useState(null);
+  const options = [];
+  props.field.options.map((option) => {
+    options.push({
+      label: option,
+      value: option,
     });
-  
-    return (
-      <MultiSelect
-        options={options}
-        value={selected}
-        required={props.field.required ? true : false}
-        className={`valid_${
-          valid != null ? (selected.length != 0 ? "true" : "false") : "0"
-          }`}
-        hasSelectAll={props.field.hasSelectAll}
-        onChange={(value) => {
-          setSelected(value);
-          setValid(true);
-          props.onChange(props.field, value);
-          props.field.value = value;
-        }}
-        labelledBy={"Select"}
-      />
-    );
-  };
+  });
 
+  useEffect(() => {
+    let sel = [];
+    if (props.value) {
+      props.value.map((val) => {
+        if (typeof val == 'string') {
+          console.log(val);
+          sel.push({
+            label: val,
+            value: val,
+          });
+        } else {
+          sel.push(val);
+        }
+      });
+      setSelected(sel);
+    }
+  }, [props.value]);
 
-  export default MultipleSelectField;
+  return (
+    <MultiSelect
+      options={options}
+      value={selected}
+      required={props.field.required ? true : false}
+      className={`valid_${
+        valid != null ? (selected.length != 0 ? 'true' : 'false') : '0'
+      }`}
+      hasSelectAll={props.field.hasSelectAll}
+      onChange={(value) => {
+        console.log(value);
+        setSelected(value);
+        setValid(true);
+        props.onChange(props.field, value);
+        props.field.value = value;
+      }}
+      labelledBy={'Select'}
+    />
+  );
+};
+
+export default MultipleSelectField;
