2eb8572d98206bdcaa51e67a7a1af4a3451b5b16
Musadiqkhan-nullbrainer <musadiq@nullbrainer.io>
2020-11-10 11:00:37 +0500

    new build

--- client/app/pages/Checkout.tsx
+++ client/app/pages/Checkout.tsx
@@ -8,7 +8,7 @@ import {
   getPIC,
 } from "utils/api-routes/api-routes.util";
 import history from "utils/history";
-import { loadStripe } from "@stripe/stripe-js";
+import { loadStripe } from "@stripe/stripe-js/pure";
 import {
   CardElement,
   Elements,
@@ -325,7 +325,14 @@ const Checkout = (props) => {
     oldValues[key] = value;
     setCheckoutInfo(oldValues);
   };
-  const stripePromise = loadStripe(appConfig.stripe);
+
+  let stripePromise;
+  const getStripe = () => {
+    if (!stripePromise) {
+      stripePromise = loadStripe(appConfig.stripe);
+    }
+    return stripePromise;
+  };
 
   const checkValidation = () => {
     setvalidation({
@@ -450,7 +457,7 @@ const Checkout = (props) => {
             ) : (
               <div className="row">
                 <div className={`form - group col - 12`}>
-                  <Elements stripe={stripePromise}>
+                  <Elements stripe={getStripe()}>
                     <CheckoutForm
                       orderid={orderid}
                       isFormSubmitted={isFormSubmitted}
