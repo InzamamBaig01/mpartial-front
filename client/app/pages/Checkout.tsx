3911f7859c67b6fb7a51119a08ef424cea3db9e9
Musadiqkhan-nullbrainer <musadiq@nullbrainer.io>
2020-11-12 14:56:34 +0500

    remove stripe lazyload package

--- client/app/pages/Checkout.tsx
+++ client/app/pages/Checkout.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useContext, useState } from "react";
-import StripeScriptLoader from "react-stripe-script-loader";
-
 import { withRouter, Link } from "react-router-dom";
 import Header from "app/components/Header";
 import { AuthContext } from "contexts/authContext";
@@ -452,24 +450,18 @@ const Checkout = (props) => {
             ) : (
               <div className="row">
                 <div className={`form - group col - 12`}>
-                  <StripeScriptLoader
-                    uniqueId="myUniqueId"
-                    script="https://js.stripe.com/v3/"
-                    loader="Loading..."
-                  >
-                    <Elements stripe={stripePromise}>
-                      <CheckoutForm
-                        orderid={orderid}
-                        isFormSubmitted={isFormSubmitted}
-                        setIsFormSubmitted={handleFormSubmittion}
-                        stripeCustomerCard={info ? info.stripeCustomerCard : []}
-                        setCardValidation={handleCardAction}
-                        cardValidation={cardValidation}
-                        checkoutInfo={checkoutInfo}
-                        PIC={PIC}
-                      />
-                    </Elements>
-                  </StripeScriptLoader>
+                  <Elements stripe={stripePromise}>
+                    <CheckoutForm
+                      orderid={orderid}
+                      isFormSubmitted={isFormSubmitted}
+                      setIsFormSubmitted={handleFormSubmittion}
+                      stripeCustomerCard={info ? info.stripeCustomerCard : []}
+                      setCardValidation={handleCardAction}
+                      cardValidation={cardValidation}
+                      checkoutInfo={checkoutInfo}
+                      PIC={PIC}
+                    />
+                  </Elements>
                   {/* <input type="checkbox" /> Card Ending 7878 */}
                 </div>
               </div>
