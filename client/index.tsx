0f163354f79c95aac5aa0f3e84d8d3c9148424d4
Musadiqkhan-nullbrainer <musadiq@nullbrainer.io>
2020-11-16 15:29:25 +0500

    new buld

--- client/index.tsx
+++ client/index.tsx
@@ -29,7 +29,7 @@ ReactDOM.render(
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/static/service-worker.js")
+      .register("/service-worker.js")
       .then((registration) => {
         console.log("SW registered: ", registration);
       })
