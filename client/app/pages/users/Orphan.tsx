3dae609b030b179f2eb73171202e9ad1396a51d9
InzamamBaig01 <inzamam@nullbrainer.io>
2021-03-01 16:48:10 +0500

    Deletion of Unused files

--- client/app/pages/users/Orphan.tsx
+++ client/app/pages/users/Orphan.tsx
@@ -15,55 +15,40 @@ const myStyle = styled.div`
   }
 `;
 
-export const Signup = () => {
+export const Orphan = () => {
   return (
     <>
       <Header isFixedColor={true}></Header>
-      <div
-        style={{
-          width: "100%",
-          minHeight: "100vh",
-          height: "100%",
-          paddingTop: "90px",
-          background: "rgba(242, 246, 247, 0.7) 0% 0% no-repeat padding-box",
-          position: "relative",
-        }}
-      >
+      <div className="helper">
         {/*  */}
         <div className="help-icon"></div>
 
-        <div className="container">
+        <div className="container offer">
           <div
-            className="row"
+            className="row offer-row"
             style={{
-              background: "#ffffff 0% 0% no-repeat padding-box",
               boxShadow: "0px 3px 6px rgb(0 0 0 / 16%)",
-              borderRadius: "5px",
-              opacity: "1",
             }}
           >
-            <div className="col-md-6 col-sm-12 col-xs-12">
-              <h4
-                style={{
-                  textAlign: "left",
-                  fontWeight: "600",
-                  fontSize: "20px",
-                  lineHeight: "24px",
-                  letterSpacing: "0px",
-                  color: "#000000",
-                  opacity: "1",
-                  characterSpacing: "0",
-                  lineSpacing: "60"
-                }}
-              >
-                It's time to take your<br></br> account to the next level.<br></br>
-             You are almost there.</h4>
+            <div className="col-md-6 col-sm-12 col-xs-12 line">
+              <h4 className="line-heading mobile-heading-one">
+                It's time to take your<br></br> account to the next level.
+                <br></br>
+                You are almost there.
+              </h4>
             </div>
-            <div className="col-md-6 col-sm-12 col-xs-12">
-              <h5>You can:</h5>
-              <h5>1.Subscribe to a Enterprise Plan</h5>
-              <h5>2.Or...be invited as a Enterprise Collobrator</h5>
+            <div className="col-md-6 col-sm-12 col-xs-12 col-two">
+              <h5 className="line-heading mobile-heading-two">You can:</h5>
+              <h5 className="mobile-center">
+                1. Subscribe to a Enterprise Plan <br></br>2. Or... be invited
+                as a Enterprise Collobrator
+              </h5>
             </div>
+          </div>
+          <div className="got-it-btn">
+            <Link to="/profile">
+              <button className="btn">GOT IT</button>
+            </Link>
           </div>
         </div>
       </div>
@@ -71,4 +56,4 @@ export const Signup = () => {
   );
 };
 
-export default withRouter(Signup);
+export default withRouter(Orphan);
